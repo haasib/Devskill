c12a49c30badcb25b33ded3855de4bbda2bc54b7
MD. Hasibul Hasan <hasibulhasan873@gmail.com>
2016-11-13 00:19:34 +0600

    Create DCP-184- Easy Sequence.c

---
+++ DCP-184- Easy Sequence.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+using namespace std;
+#define ll long long
+ll ar[45];
+int i,tc,n;
+void pregen()
+    {
+        ar[0]=0;
+        ar[1]=1;
+        ar[2]=3;
+        for(i=3;i<45;i++)ar[i]=ar[i-1]+ar[i-2];
+    }
+int main()
+    {
+        pregen();
+        scanf("%d",&tc);
+        while(tc--){
+            scanf("%d",&n);
+            printf("%d\n",ar[n]);
+        }
+        return 0;
+    }
