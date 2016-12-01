5d75ef8ed2b89b2c66e83ee2dd7e432eab871b7f
MD. Hasibul Hasan <hasibulhasan873@gmail.com>
2016-12-01 16:30:25 +0600

    Create DCP-85- Tomorrow is Eid !!.c

---
+++ DCP-85- Tomorrow is Eid !!.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+    {
+        int n;
+            scanf("%d",&n);
+            if(n==29 || n==30)
+                printf("YES\n");
+            else
+                printf("NO\n");
+        return 0;
+    }
