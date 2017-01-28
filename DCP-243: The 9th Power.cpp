8e9a7ce9a172e4c549125a82fc8869b7b7ebf2bf
MD. Hasibul Hasan <hasibulhasan873@gmail.com>
2017-01-29 00:22:24 +0600

    Create DCP-243: The 9th Power.cpp

---
+++ DCP-243: The 9th Power.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+    {
+        int tc;
+        ll n;
+        cin>>tc;
+        while(tc--){
+            cin>>n;
+            if(n%2==0)cout<<"1"<<endl;
+            else cout<<"9"<<endl;
+        }
+        return 0;
+    }
